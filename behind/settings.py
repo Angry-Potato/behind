571f6a67c45b82207c2f69d53ad8bff0aaa670ec
liam humphreys <liam.humphreys1989@gmail.com>
2019-07-25 11:51:17 +0100

    extract db config to env vars

--- behind/settings.py
+++ behind/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'behind.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres',
-        'PORT': '',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
